49c2d44b11507049b55e379077fc2df7bb17c9b4
John Mason <jmason@sciquest.com>
2016-09-03 12:44:53 -0400

    changes to support parts 1 & 2 of tutorial

---
+++ Angular-1.5/ui/src/main/resources/static/js/hello.js
@@ -0,0 +1,78 @@
+angular.module('hello', ['ngRoute'])
+	.config(function($routeProvider, $httpProvider) {
+		
+		$routeProvider.when('/', {
+			templateUrl: 'home.html',
+			controller: 'home', 
+			controllerAs: 'controller'
+		}).when('/login', {
+			templateUrl: 'login.html',
+			controller: 'navigation',
+			controllerAs: 'controller'
+		}).otherwise('/');
+		
+		$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+		
+	})
+	.controller('home', function($http) {
+		var self = this;
+		$http.get('/resource/').then(function(response) {
+			self.greeting = response.data;
+		})
+	})
+	.controller('navigation', function($rootscope, $http, $location) {
+		var self = this;
+		var authenticate = function(credentials, callback) {
+			var headers = credentials ? {authorization: "Basic" 
+				+ btoa(credentials.username + ":" + credentials.password)
+			} : {};
+		
+			$http.get('user', {headers: headers}).then(function(response) {
+				if(response.data.name) {
+					$rootscope.authenticated = true;
+				} else {
+					$rootscope.authenticated = false;
+				}
+				callback && callback();
+			}, function() {
+				$rootscope.authenticated = false;
+				callback && callback();
+			});
+		}
+		
+		authenticate();
+		self.credentials = {};
+		self.login = function() {
+			authenticate(self.credentials, function() {
+				if($rootscope.authenticated) {
+					$location.path("/");
+					self.error = false;
+				} else {
+					$location.path("/login");
+					self.error = true;
+				}
+			});
+		};
+	});
+		
+	self.logout = function() {
+		$http.post('logout', {}).finally(function() {
+			$rootscope.authenticated = false;
+			$location.path("/");
+		});
+	}
+		
+/*part 1*/
+/*angular.module('hello', [])
+	.controller('home', function($http) {
+		var self = this;
+		$http.get('/resource/').then(function(response) {
+			self.greeting = response.data;
+		})*/
+		
+		/*part 0*/
+		/*.greeting = {
+			id : 'xxx',
+			content : 'Hello World!'
+		}
+	});*/
